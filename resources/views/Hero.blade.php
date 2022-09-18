197d5d952acfc484e0a382dcf9b994af1efed391
christiansacedon3@gmail.com <christiansacedon3@gmail.com>
2022-09-18 13:43:52 -0800

    First commit

---
+++ resources/views/Hero.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MLBB</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <div class="text-center">
+    <nav class="nav nav-tabs">
+            <a href="{{url('/')}}" class="nav-link active">Home</a>
+            <a href="{{url('/History')}}" class="nav-link active">History</a>
+            <a href="{{url('/Hero')}}" class="nav-link active">Hero</a>
+        </nav>
+        <h1>My Favorite Hero</h1>
+
+        <div class="card shadow-sn mx-auto" style="width: 500px">
+            <div class="card-body">
+            <div class="card-title"><b><i></i></b></div>
+            <p>
+                <h1>Gusion</h1>
+            </p>
+                <img src="https://w0.peakpx.com/wallpaper/387/871/HD-wallpaper-gusion-cosmic-gleam-blue-neon-lights-mobile-legends-creative-legend-skin-gusion-cosmic-gleam-gusion-mobile-legends.jpg" alt="MLBB Logo" style="width:290px;height:290px;">          
+
+            </div>
+        </div>
---
+++ resources/views/History.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MLBB</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <div class="text-center">
+    <nav class="nav nav-tabs">
+            <a href="{{url('/')}}" class="nav-link active">Home</a>
+            <a href="{{url('/History')}}" class="nav-link active">History</a>
+            <a href="{{url('/Hero')}}" class="nav-link active">Hero</a>
+        </nav>
+        <h1>History</h1>
+        <div class="card shadow-sn mx-auto" style="width: 500px">
+            <div class="card-body">
+            <div class="card-title"><b><i>Mobile Legends</i></b></div>
+            <p>
+            Mobile Legends Bang Bang is developed by Moonton, a Chinese video game developer and publisher based in Shanghai. Moonton was recently acquired by Nuverse, a gaming unit by Bytedance, for around $4 million. Mobile Legends Bang Bang was released in 2016 and became very popular in Southeast Asia.
+            </p>
+            </div>
+        </div>
